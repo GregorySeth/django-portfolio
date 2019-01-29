3eb70ea74fb2e70bea0a766aa42e78380e014747
Gregory <grzegorzgieda@gmail.com>
2019-01-29 15:21:26 +0100

    usunieto zbedny model z app contact

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Contact(models.Model):
-    name = models.CharField(max_length=50)
-    mail_address = models.CharField(max_length=50)
-    message = models.TextField()
