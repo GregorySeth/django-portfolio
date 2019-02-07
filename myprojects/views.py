6b97f1879b01c63420c030dba71ae7bf53e7bd67
Gregory <grzegorzgieda@gmail.com>
2019-02-07 23:29:19 +0100

    projects order

--- myprojects/views.py
+++ myprojects/views.py
@@ -3,7 +3,7 @@ from .models import Project
 
 # Create your views here.
 def projects(request):
-    projects = Project.objects
+    projects = Project.objects.order_by('pk')
     return render(request, 'myprojects/home.html', { 'projects':projects })
 def detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
